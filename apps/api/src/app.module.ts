b9b9f91b06ad79cac89de895a6049c7618fc2af0
Ícaro Aguiar <icaroaguiar14@gmail.com>
2026-05-16 14:33:35 -0300

    feat: add user scoped positions

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AssetsModule } from "./assets/assets.module.js";
 import { MeController } from "./auth/me.controller.js";
 import { HealthController } from "./health.controller.js";
 import { PortfoliosModule } from "./portfolios/portfolios.module.js";
+import { PositionsModule } from "./positions/positions.module.js";
 
 @Module({
-	imports: [AssetsModule, PortfoliosModule],
+	imports: [AssetsModule, PortfoliosModule, PositionsModule],
 	controllers: [HealthController, MeController],
 })
 export class AppModule {}
